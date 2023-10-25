b990c3516cbecf0c770e8814cf1eba644fb76ca0
terje <tgaarden91@hotmail.com>
2023-10-25 13:35:35 +0200

    test

---
+++ #ifndef NEURON_H_.c
@@ -0,0 +1,12 @@
+#ifndef NEURON_H_
+#define NEURON_H_
+
+
+//include list
+#include <stddef.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
